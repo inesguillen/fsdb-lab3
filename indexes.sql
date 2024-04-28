ca60134935eeaf410769f7e13d9c0191f334a0c1
Inés <146125296+inesguillen@users.noreply.github.com>
2024-04-28 19:28:58 +0200

    Create indexes.sql

---
+++ indexes.sql
@@ -0,0 +1,18 @@
+-- 1st querie
+CREATE INDEX idx_posts_barcode ON posts (barcode);
+
+-- 2nd querie
+CREATE INDEX idx_posts_product ON posts (product);
+
+-- 3rd querie
+CREATE INDEX idx_posts_score ON posts (score);
+
+-- 4th querie
+CREATE INDEX idx_posts_primary_key ON posts (username, postdate);
+
+-- 5th querie
+CREATE INDEX idx_orders_clients_composite 
+  ON orders_clients (orderdate, username, town, country);
+
+CREATE INDEX idx_client_lines_composite 
+  ON client_lines (orderdate, username, town, country, barcode);
