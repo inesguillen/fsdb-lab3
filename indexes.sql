d10614899061bc26e7d43df2f8aba21862e07af2
Inés <146125296+inesguillen@users.noreply.github.com>
2024-04-29 18:18:57 +0200

    Update indexes.sql

--- indexes.sql
+++ indexes.sql
@@ -4,13 +4,13 @@ DROP INDEX idx_posts_score;
 
 
 -- 1st querie
-CREATE INDEX idx_posts_barcode ON posts (barcode) TABLESPACE TAB_16k;
+CREATE INDEX idx_posts_barcode ON posts (barcode);
 
 -- 2nd querie
 CREATE INDEX idx_posts_product ON posts (product);
 
 -- 3rd querie
-CREATE INDEX idx_posts_score ON posts (score) TABLESPACE TAB_16k;
+CREATE INDEX idx_posts_score ON posts (score);
 
 -- 4th querie
 -- CREATE INDEX idx_posts_primary_key ON posts (username, postdate);
