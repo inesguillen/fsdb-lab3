6d278007a64c7ab846eec14990846e3c634be49d
Inés <146125296+inesguillen@users.noreply.github.com>
2024-04-30 12:55:30 +0200

    Delete hints.sql

--- hints.sql
+++
@@ -1,8 +0,0 @@
--- Hint 1
-SELECT /*+ INDEX(posts idx_posts_barcode) */ * FROM Posts WHERE barcode = 'OII04455O419282';
-
--- Hint 2
-SELECT /*+ INDEX(Posts idx_posts_product) */ * FROM Posts WHERE product = 'Compromiso';
-
-
-
